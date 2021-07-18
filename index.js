03a3a6fa97dac0175e232ace7418c5a007468d48
Layna Oyervides <laynaoyervides@gmail.com>
2021-07-17 21:42:40 -0600

    Completed Assignments

---
+++ index.js
@@ -0,0 +1,24 @@
+function add (a, b){
+    return a + b;
+}
+function subtract(a,b){
+    return a - b;
+}
+function multiply(a, b){
+    return a*b ;
+}
+function divide(a, b){
+    return a/b;
+}
+function decrement(a){
+    return a - 1;
+}
+function increment(a){
+    return a + 1;
+}
+function makeInt(string){
+    return parseInt(string, 10);
+}
+function preserveDecimal(string){
+    return parseFloat(string);
+}
